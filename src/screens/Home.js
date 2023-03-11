136329b0bf604004606f00671e8e89c762078738
abdessamadbettal <abdessamadbettal@gmail.com>
2023-03-11 12:29:41 +0100

    add genre card

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,29 +1,65 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View , ScrollView } from 'react-native';
+import { StyleSheet, Text, View, ScrollView , FlatList } from 'react-native';
 import COLORS from '../constants/Colors';
+import FONTS from '../constants/Fonts';
+import GenreCard from '../components/GenreCard';
 
+
+const Genres = ["All", "Action", "Comedy", "Romance", "Horror", "sport"];
 // home screen
-const  Home = () => {
+const Home = () => {
   console.log('Hello World');
   return (
     <ScrollView style={styles.container}>
       <StatusBar style="auto" translucent={false}
-      backgroundColor={COLORS.BASIC_BACKGROUND}  />
-      <Text>POP  films</Text>
+        backgroundColor={COLORS.BASIC_BACKGROUND}
+
+      />
+      <View style={
+        styles.headerContainer
+      }>
+          <Text style={styles.headerTitle}>New movies</Text>
+          <Text style={styles.headerSubTitle}>view all</Text>
+      </View>
+      <View style={styles.genreListContainer}>
+        <FlatList
+          data={Genres}
+          horizontal
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <GenreCard genreName={item} active={item === "All"} />
+          )}
+        />
+      </View>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: COLORS.BASIC_BACKGROUND,
-    //   paddingTop: 40,
-    //   backgroundColor: '#fff',
-    //   alignItems: 'center',
-    //   justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: COLORS.BASIC_BACKGROUND,
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+  },
+  headerTitle: {
+    fontSize: 20,
+    fontWeight: FONTS.BOLD,
+    color: COLORS.BLACK,
+  },
+  headerSubTitle: {
+    fontSize: 14,
+    fontWeight: FONTS.BOLD,
+    color: COLORS.GRAY,
+
+  },
+
+});
 
 export default Home;
